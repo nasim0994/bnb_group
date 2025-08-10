9364d965e9c53c317e07a6a1406967f0aa5dcb2d
nasim0994 <nasimuddin0994@gmail.com>
2025-08-10 13:22:23 +0600

    update

--- backend/src/routes/contactRoutes.js
+++
@@ -1,9 +0,0 @@
-const router = require("express").Router();
-const verifyAdmin = require("../middleware/verifyAdmin");
-const { get, update, add } = require("../controllers/contactController");
-
-router.get("/all", get);
-router.post("/add", verifyAdmin, add);
-router.patch("/update/:id", verifyAdmin, update);
-
-module.exports = router;
